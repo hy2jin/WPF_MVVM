d48258776278485731833ef79e0f987db58a1f26
hyejin <hy2jinl22@gmail.com>
2024-04-04 16:30:48 +0900

    6.6 예제까지

---
+++ Chapter6/Chapter6/Models/Currency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter6.Models
+{
+    public class Currency
+    {
+        public string Title { get; set; }
+        public decimal Rate { get; set; }
+
+        public Currency(string title, decimal rate)
+        {
+            Title = title;
+            Rate = rate;
+        }
+    }
+}
--- Chapter6/Chapter6/ViewModels/CurrencyConverterViewModel.cs
+++ Chapter6/Chapter6/ViewModels/CurrencyConverterViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chapter6.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,31 @@ namespace Chapter6.ViewModels
 {
     public class CurrencyConverterViewModel : Notifier
     {
+        public CurrencyConverterViewModel()
+        {
+            Currencies = new List<Currency>()
+            {
+                new Currency("US Dollar", 1.1M),
+                new Currency("British Pound", 0.9M)
+            };
+        }
+
+        private void OnEurosChanged()
+        {
+            ComputeConverted();
+        }
+        private void OnSelectedCurrencyChanged()
+        {
+            ComputeConverted();
+        }
+        private void ComputeConverted()
+        {
+            if (SelectedCurrency == null) return;
+
+            Converted = Euros * SelectedCurrency.Rate;
+            ResultText = $"Amount in {SelectedCurrency.Title}";
+        }
+
         #region 유로값
         private decimal euros;
         public decimal Euros
@@ -17,28 +43,64 @@ namespace Chapter6.ViewModels
             {
                 euros = value;
                 OnPropertyChanged(nameof(Euros));
-                string tmp = nameof(Euros);
                 OnEurosChanged();
             }
         }
         #endregion
 
-        #region 달러값
-        private decimal dollars;
-        public decimal Dollars
+        #region 변환값
+        private decimal converted;
+        public decimal Converted
         {
-            get { return dollars; }
+            get { return converted; }
             set
             {
-                dollars = value;
-                OnPropertyChanged(nameof(Dollars));
+                converted = value;
+                OnPropertyChanged(nameof(Converted));
             }
         }
         #endregion
 
-        private void OnEurosChanged()
+        #region 선택된값
+        private Currency selectedCurrency;
+        public Currency SelectedCurrency
         {
-            Dollars = Euros * 1.1M;
+            get { return selectedCurrency; }
+            set
+            {
+                selectedCurrency = value;
+                OnPropertyChanged(nameof(SelectedCurrency));
+                OnSelectedCurrencyChanged();
+            }
         }
+        #endregion
+
+        #region 리스트
+        private IEnumerable<Currency> currencies;
+        public IEnumerable<Currency> Currencies
+        { 
+            get { return  currencies; }
+            set
+            {
+                currencies = value;
+                OnPropertyChanged(nameof(Currencies));
+            }
+        }
+        #endregion
+
+        #region 보여줄 값
+        private string resultText;
+        public string ResultText
+        {
+            get { return resultText; }
+            set
+            {
+                resultText = value;
+                OnPropertyChanged(nameof(ResultText));
+            }
+        }
+        #endregion
+
+
     }
 }
